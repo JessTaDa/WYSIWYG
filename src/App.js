45174fb71b12829a0049b669d275b5f283d8b3a0
Jesstada <jesstada@gmail.com>
2017-07-07 15:12:58 +1000

    output into markdown in console

--- src/App.js
+++ src/App.js
@@ -2,31 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import { Editor, config } from 'react-draft-wysiwyg';
 import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import { EditorState, ContentState, convertToRaw, convertFromRaw, getCurrentContent } from 'draft-js'
-
-
+import { editorState, contentState, convertToRaw, getCurrentContent, RichUtils} from 'draft-js'
 import draftToMarkdown from 'draftjs-to-markdown';
 
-const rawContentState = convertToRaw(EditorState.getCurrentContent());
-const markup = draftToMarkdown(ContentState, config);
-
-
 class CustomEditor extends Component {
+
+onEditorStateChange = (editorState) => {
+  const rawContentState = convertToRaw(editorState.getCurrentContent());
+  const markup = draftToMarkdown(rawContentState);
+  console.log(markup)
+}
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to the Editor</h2>
+
         </div>
         <Editor
          wrapperClassName="wrapper-class"
          editorClassName="editor-class"
          toolbarClassName="toolbar-class"
+         onEditorStateChange={this.onEditorStateChange}
         />
-
-        {/* {markup} */}
-
-
       </div>
     );
   }
